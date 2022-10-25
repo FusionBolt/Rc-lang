a42175566f866fa6a46dade6130a1d7f123d4dce
FusionBolt <homura.alive@gmail.com>
2022-10-25 20:42:58 +0800

    add DFCalculator

--- src/main/scala/mir/DFCalculator.scala
+++ src/main/scala/mir/DFCalculator.scala
@@ -3,22 +3,30 @@ package mir
 
 case class DFCalculator(var domTree: DomTree) {
   def findParents(tree: DomTree, root: DomTreeNode): Set[DomTreeNode] = {
-//    println(s"root:${root.name}")
-    val result = domTree.nodes.filter(node => {
-      if (node._2.iDom == null) {
-        false
-      } else {
-//        println(s"node:${node._1.name} -> ${node._2.iDom.name}")
-        root idom node._2
-      }
+    val map = predecessorsMap(tree.nodes.keys.toList)
+    val res = domTree.nodes.filter(node => {
+      val pres = map(node._1)
+//      println(s"current node: ${node._1.name}")
+//      println(s"preds $pres")
+      // join node -> pres.size > 1
+
+      // n -> x -> y
+      // x sdom y
+      // n dom y
+      pres.size > 1 && pres.exists(p => {
+        println(s"p: ${p.name}")
+        println(domTree(p).children.map(_.basicBlock.name))
+        (root sdom domTree(p)) && (p != node._1)
+      })
     }).values.toSet
-//    println(s"root:${root.name} end")
-    result
+    println(s"res:${res.map(_.name)}")
+    res
   }
 
   def run(bb: BasicBlock): List[BasicBlock] = {
     val parents = findParents(domTree, domTree(bb))
-    val result = parents.map(node => findParents(domTree, node)).reduce(_ | _)
-    result.map(_.basicBlock).toList
+    parents.toList.map(_.basicBlock)
+//    val result = parents.map(node => findParents(domTree, node)).reduce(_ | _)
+//    result.map(_.basicBlock).toList
   }
 }
--- src/test/scala/mir/DFCalculatorTest.scala
+++ src/test/scala/mir/DFCalculatorTest.scala
@@ -5,12 +5,58 @@ import ty.NilType
 import scala.collection.mutable.LinkedHashSet
 
 class DFCalculatorTest extends RcTestBase with MIRTestUtil {
-  describe("simple") {
+//  override val bbs = mkBBs(
+//    "entry" -> "0",
+//    "0" -> "1",
+//    "1" -> "2",
+//    "2" -> "3",
+//    "3" -> "4",
+//    "3" -> "12",
+//    "4" -> "5",
+//    "4" -> "1",
+//    "5" -> "8",
+//    "5" -> "6",
+//    "6" -> "7",
+//    "6" -> "4",
+//    "7" -> "exit",
+//    "8" -> "9",
+//    "8" -> "1",
+//    "9" -> "10",
+//    "10" -> "13",
+//    "10" -> "11",
+//    "11" -> "9",
+//    "11" -> "8",
+//    "12" -> "2",
+//    "12" -> "1",
+//    "13" -> "9",
+//    "13" -> "8",
+//  )
+
+  describe("build") {
+    val order = List(0, 1, 2, 5, 6, 8, 7, 3, 4)
+    val bbs = mkBBsByOrder(
+      order,
+      "entry" -> "0",
+      "0" -> "1",
+      "1" -> "2",
+      "2" -> "3",
+      "1" -> "5",
+      "5" -> "6",
+      "6" -> "7",
+      "5" -> "8",
+      "8" -> "7",
+      "7" -> "3",
+      "3" -> "1",
+      "3" -> "4",
+      "4" -> "exit",
+    )
     it("succeed") {
+      given BBsType = bbs
       val tree = mkTree
+      given DomTree = tree
       iDomCompute(tree, "entry")
       val result = DFCalculator(tree).run("1")
-      assert(result == List(bbs("4")))
+      assert(result.toSet == List("1", "3", "7").map(tree(_).basicBlock).toSet)
     }
   }
 }
