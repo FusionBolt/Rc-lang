58c2546684b1a19b6f6e687af37f7a497967b8d3
FusionBolt <realfusionbolt@gmail.com>
2023-04-19 21:49:00 +0800

    add rcc cmd parse

--- build.sbt
+++ build.sbt
@@ -15,6 +15,7 @@ libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.2.9" % Test,
   "org.typelevel" %% "cats-core" % "2.7.0",
   "org.typelevel" %% "cats-effect" % "3.3.9",
+  "com.github.scopt" %% "scopt" % "4.1.0",
 )
 
 libraryDependencies ++= Seq(
--- src/main/scala/Interface/RCC.scala
+++ src/main/scala/Interface/RCC.scala
@@ -1,7 +1,38 @@
 package rclang
 
 import Interface.*
-
 import compiler.CompileOption
 
-@main def rcc = compiler.Driver(CompileOption("demo/array.rc"))
+import scala.util.CommandLineParser
+import scopt.OParser
+
+given CommandLineParser.FromString[CompileOption] with {
+  override def fromString(s: String): CompileOption = {
+    val builder = OParser.builder[CompileOption]
+    val parser = {
+      import builder._
+      OParser.sequence(
+        programName("rcc"),
+        head("rcc", "0.0.0"),
+        arg[String]("<file>...")
+          .unbounded()
+          .action((x, c) => c.copy(srcPath = List(x):::c.srcPath))
+          .text("optional unbounded args"),
+        opt[String]('o', "output")
+          .text("set output file name"),
+        opt[Unit]('t', "target")
+          .text("show available target")
+          .action((_, c) => {
+            println("only suppport GNU ASM")
+            c
+          }),
+        help('h', "help").text("display available options"),
+      )
+    }
+    OParser.parse(parser, s.split(' '), CompileOption()) match
+      case Some(value) => value
+      case None => CompileOption()
+  }
+}
+
+@main def rcc(option: CompileOption) = compiler.Driver(option)
